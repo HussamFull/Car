bed4db880f052b299f70b90a9af6743cf3697424
hamada <bhak6150@gmail.com>
2026-04-09 02:01:16 +0200

    init

---
+++ src/components/about/About.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+      <section id="about" className="about">
+  <div className="container" data-aos="fade-up">
+    <div className="row">
+      <div className="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay={100}>
+        <img src="assets/img/about.png" className="img-fluid" alt />
+      </div>
+      <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay={100}>
+        <h3> </h3>
+        <h4>Eine Oase des Komforts und der Eleganz im Herzen Wiens</h4>
+        <p className="fst-italic">
+         Genießen Sie ein unvergessliches Übernachtungserlebnis in unserem Apartment, wo moderner Komfort auf den klassischen Charme Wiens trifft. Jeder Winkel des Apartments wurde sorgfältig gestaltet, um eine komfortable und warme Umgebung zu schaffen, die Sie wie zu Hause fühlen lässt.
+        </p>
+        <h4>Was dieses Apartment auszeichnet:</h4>
+        <ul>
+          <li>
+            <i className="ri-check-double-line" />
+            Erstklassige Lage: Unser Apartment befindet sich in zentraler Lage in Wien und bietet einfachen Zugang zu den wichtigsten Sehenswürdigkeiten, Restaurants und Geschäften.
+          </li>
+          <li>
+            <i className="ri-check-double-line" /> Stilvolles Innendesign: Das Apartment verfügt über ein elegantes und modernes Innendesign mit komfortablen Möbeln und hochwertigen Oberflächen.
+          </li>
+          <li>
+            <i className="ri-check-double-line" /> Komplette Ausstattung: Wir bieten alle notwendigen Annehmlichkeiten für einen komfortablen Aufenthalt, darunter eine voll ausgestattete Küche, ein modernes Badezimmer und ein gemütliches Wohnzimmer.
+          </li>
+          
+          <li>
+            <i className="ri-check-double-line" /> Atemberaubende Aussichten: Genießen Sie Panoramablicke auf die Wahrzeichen Wiens aus den Fenstern des Apartments.
+          </li>
+           <li>
+            <i className="ri-check-double-line" />           Privatsphäre und Ruhe: Das Apartment bietet eine ruhige und private Atmosphäre, die Ihnen Entspannung nach einem langen Erkundungstag garantiert.
+
+          </li>
+        </ul>
+        <br />
+        <p />
+      </div>
+    </div>
+  </div>
+</section>
+
+    <div>
+
+  {/* ======= Features Section ======= */}
+  <section id="features" className="features">
+    <div className="container" data-aos="fade-up">
+      <div className="row">
+        <div className="image col-lg-6" style={{backgroundImage: 'url("assets/img/features.png")'}} data-aos="fade-right" />
+       
+       
+        <div className="col-lg-6" data-aos="fade-left" data-aos-delay={100}>
+          <h4>Taxidienst in Wien und Umgebung: Ein unvergessliches Reiseerlebnis</h4>
+          <p>Genießen Sie ein komfortables und sicheres Reiseerlebnis in Wien und Umgebung mit unserem Taxidienst. Wir bieten eine Flotte von modernen Fahrzeugen und erfahrenen Fahrern, um sicherzustellen, dass Sie pünktlich und bequem an Ihr Ziel gelangen.</p>
+         <h4>Was unseren Taxidienst auszeichnet:</h4>
+          <div className="icon-box mt-5 mt-lg-0" data-aos="zoom-in" data-aos-delay={150}>
+            <i className="bx bx-receipt" />
+            <h4>Moderne und komfortable Fahrzeuge</h4>
+            <p>
+              Wir bieten eine Vielzahl von modernen und komfortablen Fahrzeugen, darunter Limousinen, SUVs und Minivans, um Ihren unterschiedlichen Bedürfnissen gerecht zu werden.
+            </p>
+          </div>
+          <div className="icon-box mt-5" data-aos="zoom-in" data-aos-delay={150}>
+            <i className="bx bx-cube-alt" />
+            <h4>Erfahrene Fahrer</h4>
+            <p>
+              Unsere Fahrer verfügen über umfangreiche Erfahrung im Fahren in Wien und Umgebung und achten darauf, Ihnen ein sicheres und komfortables Reiseerlebnis zu bieten.
+            </p>
+          </div>
+          <div className="icon-box mt-5" data-aos="zoom-in" data-aos-delay={150}>
+            <i className="bx bx-images" />
+            <h4>Vernünftige Preise</h4>
+            <p>
+Wir bieten vernünftige und transparente Preise mit verschiedenen Zahlungsoptionen, um Ihrem Budget gerecht zu werden.            </p>
+          </div>
+          <div className="icon-box mt-5" data-aos="zoom-in" data-aos-delay={150}>
+            <i className="bx bx-shield" />
+            <h4> Umfassende Abdeckung</h4>
+            <p>
+Wir bieten Taxidienst in Wien und Umgebung, einschließlich der umliegenden Gebiete und anderer Städte.            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/* End Features Section */}
+</div>
+
+    </>
+  )
+}
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default function Navbar() {
+  const [mobileNavActive, setMobileNavActive] = useState(false);
+
+  const handleMobileNavToggle = () => {
+    setMobileNavActive(!mobileNavActive);
+  };
+
+  return (
+    <>
+      <header id="header" className="fixed-top header-scrolled"> 
+        <div className="container d-flex align-items-center justify-content-lg-between">
+          <h1 className="logo me-auto me-lg-0">
+            <Link to={"/"}> 
+<img src="/assets/img/utg-logo.png" alt="UTG Logo" className="img-fluid"
+ //style={{ width: '100px', height: 'auto' }}
+  />
+
+</Link>
+          </h1>
+          <nav id="navbar" className={`navbar order-last order-lg-0 ${mobileNavActive ? 'navbar-mobile' : ''}`}>
+            <ul>
+              <li><Link to={""} className="nav-link scrollto active" href="#hero">Home</Link></li>
+              <li><Link to={"About"} className="nav-link scrollto" href="#about">About</Link></li>
+
+             <ul>
+  <li class="nav-item dropdown">
+    <Link to="/Services" class="nav-link dropdown-toggle" href="#services" id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      Services
+    </Link>
+    <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
+      <li><Link to="/Apartment" class="dropdown-item" href="#apartment">Apartment</Link></li>
+      <li><Link to="/Car" class="dropdown-item" href="#car">Car</Link></li>
+    </ul>
+  </li>
+</ul>
+
+              <li>
+    
+    
+                <Link to={"Portfolio"} className="nav-link scrollto" href="#portfolio">Portfolio</Link>
+              </li>
+              <li><Link to={"Team"} className="nav-link scrollto" href="#team">Team</Link></li>
+              <li><Link to={"Contact"} className="nav-link scrollto" href="#contact">Contact</Link></li>
+            </ul>
+            <i 
+              className={`bi bi-list mobile-nav-toggle ${mobileNavActive ? 'bi-x' : ''}`}
+              onClick={handleMobileNavToggle}
+            />
+          </nav>
+          <a href="https://wa.me/message/" className="get-started-btn scrollto">Kontaktieren Sie uns</a>
+        
+
+        </div>
+      </header>
+    </>
+  );
+}
